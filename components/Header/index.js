9364abf3b7395ef479d9034739b3a9e423fa9450
mxxt1 <matt@dustcloudlabs.com>
2019-09-06 11:47:32 -0400

    Header complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,37 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+
+function Header(){
+    //create elements
+    const container = document.createElement('div');
+        const topSpan = document.createElement('span');
+        const h1 = document.createElement('h1');
+        const bottomSpan = document.createElement('span');
+
+    //setup structure
+
+    container.appendChild(topSpan);
+    container.appendChild(h1);
+    container.appendChild(bottomSpan);
+
+    //assign classes
+
+    container.classList.add('header');
+    topSpan.classList.add('date');
+    bottomSpan.classList.add('temp');
+
+
+    //set content
+
+    topSpan.textContent = 'March 28,2019';
+    h1.textContent = 'Lambda Times';
+    bottomSpan.textContent = '98°';
+
+return container;
+
+}
+
+headerContainer.appendChild(Header());
